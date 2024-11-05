fd7bbf1e80a8afa00a4433939617e467dde2c7a3
unknown <mimisaki008@gmail.com>
2024-11-05 21:47:31 +0800

    Update UI

--- resources/views/Layouts/inc/guestFooter.blade.php
+++ resources/views/Layouts/inc/guestFooter.blade.php
@@ -1,21 +1,21 @@
-<footer style="color: #000; padding: 15px;">
+<footer style="color: #000; padding: 7px;">
     <div class="container">
         <div class="row align-items-center">
             <!-- Logo Section -->
             <div class="col-md-4 text-center text-md-start" style="display: flex; align-items: center;">
                 <img src="{{ asset('images/logo.png') }}" alt="Logo" style="width: 100px; height: auto; margin-right: 10px;">
-                <h5 style="margin: 0; font-weight: bold;">Kyla and Kyle</h5> <!-- Title beside the logo -->
+                <h5 style="margin: 0; font-weight: bold; color: white; ">Kyla and Kyle</h5> <!-- Title beside the logo -->
             </div>
 
             <!-- Contact Information and Links -->
             <div class="col-md-4 text-center text-md-start" style="text-align: left;">
-                <p style="margin: 0; font-weight: bold; text-align: center;">
+                <p style="margin: 0; font-weight: bold; text-align: center; color: white;">
                     <i class="bi bi-geo-alt-fill me-2"  style="color: darkorange;"></i> 428 Cacho St. Brgy Balite Rodriguez Rizal
                 </p>
-                <p style="margin: 5px 0; text-align: center; font-weight: bold;">
+                <p style="margin: 5px 0; text-align: center; font-weight: bold; color: white;">
                 <i class="bi bi-telephone-fill me-2"  style="color: darkorange;"></i> 0917-82-1971 &nbsp;&nbsp; <i class="bi bi-telephone-fill me-2"  style="color: darkorange;"></i> 0945-413-0258
                 </p>
-                <p style="margin-top: 5px; font-size: 14px; text-align: center; font-weight: bold;">
+                <p style="margin-top: 5px; font-size: 14px; text-align: center; font-weight: bold; color: white;">
                     &copy; {{ date('Y') }} Kyla and Kyle Catering Services. All Rights Reserved.
                 </p>
             </div>
